3f808101ead8fb6ff2f048d5f3e643b6d6d19e89
AnilKManthena <akmanthena@gmail.com>
2024-04-21 18:50:35 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -11,12 +11,13 @@ N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-        echo -e "$2...$R FAILURE $N"
-        exit 1
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit
     else
-        echo -e "$2...$G SUCCESS $N"
-    }
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
 
 if [ $USERID -ne 0 ]
 then 
