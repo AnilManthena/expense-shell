3bf8e7b6f3699c00db322601fb43def07230931f
AnilKManthena <akmanthena@gmail.com>
2024-04-25 00:27:29 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -8,45 +8,43 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-echo "Please enter DB Password:"
-read -s mysql_root_password
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
+VALIDATE(){ 
+if [ $1 -ne 0 ]
+then 
+    echo -e  "$2...$R FAILURE $N"
+    exit 1
+else
+    echo -e  "$2...$G SUCCESS $N"
+fi
 }
 
 if [ $USERID -ne 0 ]
-then 
-    echo "Please run this script with root access."
-    exit 1
-else
-    echo "You are super user."
-fi
+    then
+        echo  "Please run this script with root access."
+        exit 1
+    else
+        echo  "you are super user."
+    fi
 
-dnf install mysql-server -y &>>$LOGFILE 
-VALIDATE $? "Installing MySql Server"
 
-systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling mysql server"
+    dnf install mysql-server -y &>>$LOGFILE
+    VALIDATE $? "Installing MYSQL Server."
 
-systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting mysql server"
+    systemctl enable mysqld &>>$LOGFILE
+    VALIDATE $? "Enabling MYSQL Server."
 
-# mysql_secure_installation --set-root-pass -p${mysql_root_password} &>>$LOGFILE
-# VALIDATE $? "Settingup root password"
+    systemctl start mysqld &>>LOGFILE
+    VALIDATE $? "Starting MYSQL Server."
 
-#below code is useful for idempotent nature
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE 
+    VALIDATE $? "Settingup root password."
 
-mysql -h db.devops4me.cloud -uroot -p${mysql_root_password} -e 'SHOW DATABASES;' &>>$LOGFILE
+
+    mysql -h db.devops4me.cloud -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass -p${mysql_root_password} &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
     VALIDATE $? "MySQL root password setup"
 else
     echo -e "MySQL root password is already setup...  $Y SKIPPING $N"
