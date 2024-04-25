b54e1fc5f482edb5496e601bef1ff41dbb38e51b
AnilKManthena <akmanthena@gmail.com>
2024-04-25 10:55:46 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -10,74 +10,75 @@ Y="\e[33m"
 N="\e[0m"
 echo "Please enter DB password:"
 read -s mysql_root_password
-   
+
 VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo -e "$2 ... $G SUCCESS $N"
+        echo -e "$2...$G SUCCESS $N"
     fi
 }
 
 if [ $USERID -ne 0 ]
-then 
+then
     echo "Please run this script with root access."
-    exit 1
+    exit 1 # manually exit if error comes.
 else
     echo "You are super user."
 fi
 
-dnf module disable nodejs -y &>>$LOGFILE 
+dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "Disabling default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOGFILE
 VALIDATE $? "Enabling nodejs:20 version"
 
 dnf install nodejs -y &>>$LOGFILE
-VALIDATE $? "installing nodejs"
+VALIDATE $? "Installing nodejs"
 
 id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     useradd expense &>>$LOGFILE
-    VALIDATE $? "creating expense user"
-else    
-    echo -e "expense user already created ... $Y SKIPPING $N"
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>$LOGFILE
-VALIDATE $? "creating app directory"
+VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading backend code"
 
-cd /app 
+cd /app
 rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
-VALIDATE $? "extracted backend code"
+VALIDATE $? "Extracted backend code"
 
 npm install &>>$LOGFILE
-VALIDATE $? "installing nodejs dependencies"
+VALIDATE $? "Installing nodejs dependencies"
 
+#check your repo and path
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
-VALIDATE $? "copied backend sevice"
+VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "daemon-reload"
+VALIDATE $? "Daemon Reload"
 
 systemctl start backend &>>$LOGFILE
-VALIDATE $? "starting backend"
+VALIDATE $? "Starting backend"
 
 systemctl enable backend &>>$LOGFILE
-VALIDATE $? "backend enabling"
+VALIDATE $? "Enabling backend"
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing mysql client"
+VALIDATE $? "Installing MySQL Client"
 
 mysql -h db.devops4me.cloud -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
-VALIDATE $? "restarting backend" 
+VALIDATE $? "Restarting Backend"
